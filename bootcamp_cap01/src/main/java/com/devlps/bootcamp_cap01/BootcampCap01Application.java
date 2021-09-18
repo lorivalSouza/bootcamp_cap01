5dcee2ec6cbc48d2b2431d880e06b3deebb5d1a3
lorivalSouza <lorivalnew@gmail.com>
2021-09-18 12:56:31 -0300

    initial commit

---
+++ bootcamp_cap01/src/main/java/com/devlps/bootcamp_cap01/BootcampCap01Application.java
@@ -0,0 +1,13 @@
+package com.devlps.bootcamp_cap01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampCap01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampCap01Application.class, args);
+	}
+
+}
---
+++ bootcamp_cap01/src/test/java/com/devlps/bootcamp_cap01/BootcampCap01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlps.bootcamp_cap01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampCap01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
